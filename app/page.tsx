b110d64a1355f889ca6d3ff75649f67e675f2b58
James Barker <j404404404404@gmail.com>
2026-03-11 20:14:00 +0000

    Changed google maps library to a more flexible option

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,27 @@
-import { GoogleMapsEmbed } from '@next/third-parties/google'
+'use client'
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 export default function Home() {
-  var location = undefined;
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY || ""
+  })
+
+  var location = { lat: 43, lng: 177 };
   const geolocation = navigator.geolocation;
-  geolocation?.getCurrentPosition((position)=>{
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    location = `${lat}, ${lng}`
-    location = `${43}, ${177}`
+  geolocation?.getCurrentPosition((position) => {
+    location.lat = position.coords.latitude;
+    location.lng = position.coords.longitude;
   });
-  return location == undefined ? (
-    <GoogleMapsEmbed
-      apiKey="AIzaSyCg0OB3NMGcGkGIhBKodyMwVv2rrrPFXZ0"
-      height="100%"
-      width="100%"
-      mode="place"
-    /> 
-  ) : (<GoogleMapsEmbed
-      apiKey="AIzaSyCg0OB3NMGcGkGIhBKodyMwVv2rrrPFXZ0"
-      height="100%"
-      width="100%"
-      mode="place"
-      q={location}
-    />)
+  return !isLoaded ? (
+    <span>Loading</span>
+  ) : (
+    Map(location)
+  )
+}
+
+function Map(location: { lat: number, lng: number }) {
+  return <GoogleMap zoom={10} center={location} mapContainerClassName="google-map-container">
+    <Marker position={location}></Marker>
+  </GoogleMap>
 }
